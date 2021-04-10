a883487e616e68d017e401f779b34ad54025bb94
Deisel <74831846+Amelia-was@users.noreply.github.com>
2021-04-10 11:47:22 -0400

    thought schema

---
+++ models/Thought.js
@@ -0,0 +1,40 @@
+const { Schema, model, Types } = require('mongoose');
+
+const ReactionSchema = new Schema(
+
+);
+
+const ThoughtSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true
+        },
+        thoughtText: {
+            type: String,
+            required: true,
+            minLength: 1,
+            maxLength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now
+        },
+        reactions: [ReactionSchema]
+    },
+    {
+        toJSON: {
+            getters: true,
+            virtuals: true
+        },
+        id: false
+    }
+);
+
+ThoughtSchema.virtual('reactionCount').get(function () {
+    return this.replies.length;
+});
+
+const Thought = model('Thought', ThoughtSchema);
+
+module.exports = Thought;
