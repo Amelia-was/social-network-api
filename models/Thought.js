6133d09b8d41b9481f704cf75c2636a34fbeb842
Deisel <74831846+Amelia-was@users.noreply.github.com>
2021-04-10 11:50:58 -0400

    reaction schema

--- models/Thought.js
+++ models/Thought.js
@@ -1,7 +1,30 @@
 const { Schema, model, Types } = require('mongoose');
 
 const ReactionSchema = new Schema(
-
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxLength: 280
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            defualt: Date.now
+        }
+    },
+    {
+        toJSON: {
+            getters: true
+        }
+    }
 );
 
 const ThoughtSchema = new Schema(
